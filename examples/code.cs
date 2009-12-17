2d65d3d73b9994f839c0d3de7ea0ccb2ec88ad37
Jeremy Ashkenas <jashkenas@gmail.com>
2009-12-17 08:23:07 -0500

    multiline strings

--- examples/code.cs
+++ examples/code.cs
@@ -122,3 +122,8 @@ wednesday: => eat_breakfast(); go_to_work(); eat_dinner(); .
 # Array slice literals.
 zero_to_nine: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 three_to_six: zero_to_nine[3, 6]
+
+# Multiline strings.
+story: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit,
+sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna
+aliquam erat volutpat. Ut wisi enim ad."
