849f0e419297d36e02c7a2cef2e94bdfbf2d7c71
Jeremy Ashkenas <jashkenas@gmail.com>
2009-12-24 00:49:11 -0800

    added comprehensive linting to the test suit

--- test/unit/test_execution.rb
+++ test/unit/test_execution.rb
@@ -2,6 +2,8 @@ require 'test_helper'
 
 class ExecutionTest < Test::Unit::TestCase
 
+  NO_WARNINGS = /\A(0 error\(s\), 0 warning\(s\)\n)+\Z/
+
   def test_execution_of_coffeescript
     `bin/coffee-script test/fixtures/execution/*.cs`
     sources = Dir['test/fixtures/execution/*.js'].map {|f| File.expand_path(f) }
@@ -13,6 +15,16 @@ class ExecutionTest < Test::Unit::TestCase
     end
   ensure
     Dir.chdir(starting_place)
+  end
+
+  def test_lintless_coffeescript
+    lint_results = `bin/coffee-script -l test/fixtures/execution/*.cs`
+    assert lint_results.match(NO_WARNINGS)
+  end
+
+  def test_lintless_examples
+    lint_results = `bin/coffee-script -l examples/*.cs`
+    assert lint_results.match(NO_WARNINGS)
   end
 
 end
