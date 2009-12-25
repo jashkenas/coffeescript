859ab7583fbbce4f534c0bebdb6001a7aebfb765
Jeremy Ashkenas <jashkenas@gmail.com>
2009-12-25 00:16:56 -0800

    bumping to 0.1.3 ... here we go

--- documentation/js/array_comprehensions.js
+++ documentation/js/array_comprehensions.js
@@ -2,18 +2,18 @@
 
   // Eat lunch.
   var lunch;
-  var a = ['toast', 'cheese', 'wine'];
-  var d = [];
-  for (var b=0, c=a.length; b<c; b++) {
-    var food = a[b];
-    d[b] = food.eat();
+  var __a = ['toast', 'cheese', 'wine'];
+  var __d = [];
+  for (var __b=0, __c=__a.length; __b<__c; __b++) {
+    var food = __a[__b];
+    __d[__b] = food.eat();
   }
-  lunch = d;
+  lunch = __d;
   // Zebra-stripe a table.
-  var e = table;
-  for (var f=0, g=e.length; f<g; f++) {
-    var row = e[f];
-    var i = f;
+  var __e = table;
+  for (var __f=0, __g=__e.length; __f<__g; __f++) {
+    var row = __e[__f];
+    var i = __f;
     i % 2 === 0 ? highlight(row) : null;
   }
 })();
