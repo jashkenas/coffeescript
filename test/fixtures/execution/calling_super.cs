1c83e68292270ddfef3d87da77b4ff5861bfbe26
Jeremy Ashkenas <jashkenas@gmail.com>
2009-12-24 16:49:23 -0800

    got extends back in the language -- use it together with super

--- test/fixtures/execution/calling_super.cs
+++ test/fixtures/execution/calling_super.cs
@@ -3,17 +3,17 @@ Base.prototype.func: string =>
   'zero/' + string.
 
 FirstChild: => .
-FirstChild.prototype.__proto__: new Base()
+FirstChild extends new Base()
 FirstChild.prototype.func: string =>
   super('one/') + string.
 
 SecondChild: => .
-SecondChild.prototype.__proto__: new FirstChild()
+SecondChild extends new FirstChild()
 SecondChild.prototype.func: string =>
   super('two/') + string.
 
 ThirdChild: => .
-ThirdChild.prototype.__proto__: new SecondChild()
+ThirdChild extends new SecondChild()
 ThirdChild.prototype.func: string =>
   super('three/') + string.
 
