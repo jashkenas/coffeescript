d8ceb3b4bb0c941fbec9ea818e3c63aac9f1adfa
Jeremy Ashkenas <jashkenas@gmail.com>
2009-12-24 22:29:30 -0800

    sped up the execution test a good deal by running it all in one pass

--- test/unit/test_execution.rb
+++ test/unit/test_execution.rb
@@ -3,13 +3,11 @@ require 'test_helper'
 class ExecutionTest < Test::Unit::TestCase
 
   NO_WARNINGS = /\A(0 error\(s\), 0 warning\(s\)\n)+\Z/
+  ALLS_WELL   = /\A\n?(true\n)+\Z/
 
   def test_execution_of_coffeescript
-    Dir['test/fixtures/execution/*.cs'].each do |source|
-      suceeded = `bin/cs #{source}`.chomp.to_sym == :true
-      puts "failed: #{source}" unless suceeded
-      assert suceeded
-    end
+    sources = ['test/fixtures/execution/*.cs'].join(' ')
+    assert `bin/cs #{sources}`.match(ALLS_WELL)
   end
 
   def test_lintless_coffeescript
