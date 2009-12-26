834442148bae7dd6816868d8377bf4445c6b7102
Jeremy Ashkenas <jashkenas@gmail.com>
2009-12-25 23:17:34 -0800

    docs for assignment-as-expression

--- documentation/js/overview.js
+++ documentation/js/overview.js
@@ -1,20 +1,20 @@
 (function(){
-
+  var __a, __b, __c, __d, cubed_list, list, math, num, number, opposite_day, square;
   // Assignment:
-  var number = 42;
-  var opposite_day = true;
+  number = 42;
+  opposite_day = true;
   // Conditions:
   if (opposite_day) {
     number = -42;
   }
   // Functions:
-  var square = function(x) {
+  square = function(x) {
     return x * x;
   };
   // Arrays:
-  var list = [1, 2, 3, 4, 5];
+  list = [1, 2, 3, 4, 5];
   // Objects:
-  var math = {
+  math = {
     root: Math.sqrt,
     square: square,
     cube: function(x) {
@@ -22,11 +22,10 @@
     }
   };
   // Array comprehensions:
-  var cubed_list;
-  var __a = list;
-  var __d = [];
-  for (var __b=0, __c=__a.length; __b<__c; __b++) {
-    var num = __a[__b];
+  __a = list;
+  __d = [];
+  for (__b=0, __c=__a.length; __b<__c; __b++) {
+    num = __a[__b];
     __d[__b] = math.cube(num);
   }
   cubed_list = __d;
--- documentation/js/slices.js
+++ documentation/js/slices.js
@@ -1,5 +1,6 @@
 (function(){
-  var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-  var three_to_six = numbers.slice(3, 6 + 1);
-  var numbers_copy = numbers.slice(0, numbers.length);
+  var numbers, numbers_copy, three_to_six;
+  numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+  three_to_six = numbers.slice(3, 6 + 1);
+  numbers_copy = numbers.slice(0, numbers.length);
 })();
