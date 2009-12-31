370d05148d71cd104f0d1a391e6aedd25c06bc91
Jeremy Ashkenas <jashkenas@gmail.com>
2009-12-30 21:51:23 -0500

    getting there, finally ... all tests are green for whitespace

--- test/unit/test_lexer.rb
+++ test/unit/test_lexer.rb
@@ -7,45 +7,47 @@ class LexerTest < Test::Unit::TestCase
   end
 
   def test_lexing_an_empty_string
-    assert @lex.tokenize("") == []
+    assert @lex.tokenize("") == [["\n", "\n"]]
   end
 
   def test_lexing_basic_assignment
-    code = "a: 'one'; b: [1, 2]"
-    assert @lex.tokenize(code) == [[:IDENTIFIER, "a"], [:ASSIGN, ":"],
-      [:STRING, "'one'"], [";", ";"], [:IDENTIFIER, "b"], [:ASSIGN, ":"],
-      ["[", "["], [:NUMBER, "1"], [",", ","], [:NUMBER, "2"], ["]", "]"]]
+    code = "a: 'one'\nb: [1, 2]"
+    assert @lex.tokenize(code) == [[:IDENTIFIER, "a"], [:ASSIGN, ":"], 
+      [:STRING, "'one'"], ["\n", "\n"], [:IDENTIFIER, "b"], [:ASSIGN, ":"], 
+      ["[", "["], [:NUMBER, "1"], [",", ","], [:NUMBER, "2"], ["]", "]"], 
+      ["\n", "\n"]]
   end
 
   def test_lexing_object_literal
     code = "{one : 1}"
     assert @lex.tokenize(code) == [["{", "{"], [:IDENTIFIER, "one"], [:ASSIGN, ":"],
-      [:NUMBER, "1"], ["}", "}"]]
+      [:NUMBER, "1"], ["}", "}"], ["\n", "\n"]]
   end
 
   def test_lexing_function_definition
-    code = "x, y => x * y."
+    code = "x, y => x * y"
     assert @lex.tokenize(code) == [[:PARAM, "x"], [",", ","], [:PARAM, "y"],
-      ["=>", "=>"], [:IDENTIFIER, "x"], ["*", "*"], [:IDENTIFIER, "y"], [".", "."]]
+      ["=>", "=>"], [:INDENT, 2], [:IDENTIFIER, "x"], ["*", "*"], 
+      [:IDENTIFIER, "y"], [:OUTDENT, 2], ["\n", "\n"]]
   end
 
   def test_lexing_if_statement
     code = "clap_your_hands() if happy"
     assert @lex.tokenize(code) == [[:IDENTIFIER, "clap_your_hands"], ["(", "("],
-      [")", ")"], [:IF, "if"], [:IDENTIFIER, "happy"]]
+      [")", ")"], [:IF, "if"], [:IDENTIFIER, "happy"], ["\n", "\n"]]
   end
 
   def test_lexing_comment
-    code = "a: 1\n # comment\n # on two lines\nb: 2"
+    code = "a: 1\n# comment\n# on two lines\nb: 2"
     assert @lex.tokenize(code) == [[:IDENTIFIER, "a"], [:ASSIGN, ":"], [:NUMBER, "1"],
       ["\n", "\n"], [:COMMENT, [" comment", " on two lines"]], ["\n", "\n"],
-      [:IDENTIFIER, "b"], [:ASSIGN, ":"], [:NUMBER, "2"]]
+      [:IDENTIFIER, "b"], [:ASSIGN, ":"], [:NUMBER, "2"], ["\n", "\n"]]
   end
 
   def test_lexing_newline_escaper
     code = "two: 1 + \\\n\n 1"
     assert @lex.tokenize(code) == [[:IDENTIFIER, "two"], [:ASSIGN, ":"],
-      [:NUMBER, "1"], ["+", "+"], [:NUMBER, "1"]]
+      [:NUMBER, "1"], ["+", "+"], [:NUMBER, "1"], ["\n", "\n"]]
   end
 
   def test_lexing
