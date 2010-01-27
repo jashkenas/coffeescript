2f3a94678fc5847653b31a8125d2e7f1838c8366
Jeremy Ashkenas <jashkenas@gmail.com>
2010-01-27 07:55:40 -0500

    fixing instanceof, with a quick test

--- lib/coffee_script/rewriter.rb
+++ lib/coffee_script/rewriter.rb
@@ -22,7 +22,7 @@ module CoffeeScript
     IMPLICIT_FUNC = [:IDENTIFIER, :SUPER, ')', :CALL_END, ']', :INDEX_END]
     IMPLICIT_END  = [:IF, :UNLESS, :FOR, :WHILE, "\n", :OUTDENT]
     IMPLICIT_CALL = [:IDENTIFIER, :NUMBER, :STRING, :JS, :REGEX, :NEW, :PARAM_START,
-                     :TRY, :DELETE, :INSTANCEOF, :TYPEOF, :SWITCH, :ARGUMENTS,
+                     :TRY, :DELETE, :TYPEOF, :SWITCH, :ARGUMENTS,
                      :TRUE, :FALSE, :YES, :NO, :ON, :OFF, '!', '!!', :NOT,
                      '->', '=>', '[', '(', '{']
 
--- test/fixtures/execution/test_functions.coffee
+++ test/fixtures/execution/test_functions.coffee
@@ -4,6 +4,7 @@ y.x: -> 3
 
 print x is 1
 print typeof(y.x) is 'function'
+print y.x instanceof Function
 print y.x() is 3
 print y.x.name is 'x'
 
