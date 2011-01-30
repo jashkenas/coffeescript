49d9eb687d65a5c12cdb3ff8f263d6ec3c7935b6
Jeremy Ashkenas <jashkenas@gmail.com>
2011-01-30 17:10:50 -0500

    documenting 'when', a bit.

--- documentation/js/do.js
+++ documentation/js/do.js
@@ -1,10 +1,10 @@
-var fileName, _fn, _i, _len;
-_fn = function(fileName) {
-  return fs.readFile(fileName, function(err, contents) {
-    return compile(fileName, contents.toString());
+var filename, _fn, _i, _len;
+_fn = function(filename) {
+  return fs.readFile(filename, function(err, contents) {
+    return compile(filename, contents.toString());
   });
 };
 for (_i = 0, _len = list.length; _i < _len; _i++) {
-  fileName = list[_i];
-  _fn(fileName);
+  filename = list[_i];
+  _fn(filename);
 }
