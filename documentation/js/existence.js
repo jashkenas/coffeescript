47f12c453aaa2b9e2c58aa023f7dde4a2ce1c10e
Michael Ficarra <git@michael.ficarra.me>
2011-06-08 19:38:12 -0400

    subliminally promoting the use of interpolation in the documentation

--- documentation/js/existence.js
+++ documentation/js/existence.js
@@ -2,9 +2,7 @@ var footprints, solipsism;
 if ((typeof mind !== "undefined" && mind !== null) && !(typeof world !== "undefined" && world !== null)) {
   solipsism = true;
 }
-if (typeof speed !== "undefined" && speed !== null) {
-  speed;
-} else {
+if (typeof speed === "undefined" || speed === null) {
   speed = 75;
 };
 footprints = typeof yeti !== "undefined" && yeti !== null ? yeti : "bear";
