43e8478cd55bd0db7ad199854511e1af058861db
Maxwell Krohn <themax@gmail.com>
2012-01-05 18:36:59 -0500

    some progress here, finally groked how it's working.  need to figure out something fancy to do with the boilerplate.

--- documentation/js/array_comprehensions.js
+++ documentation/js/array_comprehensions.js
@@ -1,4 +1,4 @@
-var courses, dish, food, foods, i, _i, _j, _len, _len2, _len3, _ref;
+var courses, dish, food, foods, i, _i, _j, _k, _len, _len2, _len3, _ref;
 
 _ref = ['toast', 'cheese', 'wine'];
 for (_i = 0, _len = _ref.length; _i < _len; _i++) {
@@ -8,14 +8,14 @@ for (_i = 0, _len = _ref.length; _i < _len; _i++) {
 
 courses = ['greens', 'caviar', 'truffles', 'roast', 'cake'];
 
-for (i = 0, _len2 = courses.length; i < _len2; i++) {
+for (i = _j = 0, _len2 = courses.length; _j < _len2; i = ++_j) {
   dish = courses[i];
   menu(i + 1, dish);
 }
 
 foods = ['broccoli', 'spinach', 'chocolate'];
 
-for (_j = 0, _len3 = foods.length; _j < _len3; _j++) {
-  food = foods[_j];
+for (_k = 0, _len3 = foods.length; _k < _len3; _k++) {
+  food = foods[_k];
   if (food !== 'chocolate') eat(food);
 }
