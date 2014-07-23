e8c96de269fe629b17f7f530a0baaf93eef152fc
Nami-Doc <vendethiel@hotmail.fr>
2014-07-23 22:17:00 +0200

    Fix the "Examples" link on coffeescript.org
    It now points to the "Trending repositories" of the month.

--- documentation/index.html.js
+++ documentation/index.html.js
@@ -1082,7 +1082,7 @@ Expressions
     </h2>
 
     <p>
-      The <a href="https://github.com/languages/coffeescript">best list of
+      The <a href="https://github.com/trending?l=coffeescript&amp;since=monthly">best list of
       open-source CoffeeScript examples</a> can be found on GitHub. But just
       to throw out few more:
     </p>
