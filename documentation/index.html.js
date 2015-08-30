874628b1d0cf608601de482df8d47f42cdc39340
Simon Lydell <simon.lydell@gmail.com>
2015-08-30 12:09:02 +0200

    Improve the documentation site on smaller screens
    "Smaller screens" means screens smaller than 820px wide. That's the smallest
    width that the current design looks good at.
    
    I tried to not change the current design in any way and to make as few changes
    as possible.
    
    This is what happens on smaller screens:
    
    - The navigation bar is no longer fixed to the viewport, but always at the top
      of the page.
    - The navigation bar is vertical rather than horizontal.
    - CoffeeScript code is above the compiled JavaScript rather than having them
      side by side.
    
    This allows for a larger text size, which is more compfortable to read, and no
    horizontal scrolling, which many find difficult to use.

--- documentation/index.html.js
+++ documentation/index.html.js
@@ -3,6 +3,7 @@
 <head>
   <meta http-equiv="content-type" content="text/html;charset=UTF-8" />
   <title>CoffeeScript</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
   <link rel="canonical" href="http://coffeescript.org" />
   <link rel="stylesheet" type="text/css" href="documentation/css/docs.css" />
   <link rel="stylesheet" type="text/css" href="documentation/css/tomorrow.css" />
