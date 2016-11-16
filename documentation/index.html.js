3b0b002f298a4a372fdbcc68a964c27c51f17934
Geoffrey Booth <webmaster@geoffreybooth.com>
2016-11-15 20:37:11 -0800

    Use Google-hosted jQuery

--- documentation/index.html.js
+++ documentation/index.html.js
@@ -2372,7 +2372,7 @@ six = -&gt;
 
   </script>
 
-  <script src="documentation/vendor/jquery-1.6.4.js"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
   <script src="extras/coffee-script.js"></script>
 
 </body>
