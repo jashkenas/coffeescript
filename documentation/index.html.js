6c759d49bde9ba478ab3b98a656c13efb22aa7e8
Daniel Bayley <daniel.bayley@me.com>
2016-11-21 07:04:49 +0000

    Docs shorthand object notation (#4356)
    * Docs shorthand object notation
    
    A simple but slightly more imaginative example of shorthand object notation.
    
    Closes #1808.
    
    Signed-off-by: Daniel Bayley <daniel.bayley@me.com>
    
    * Update for new documentation folder structure
    
    * Fix typo

--- documentation/index.html.js
+++ documentation/index.html.js
@@ -457,6 +457,11 @@ Block
       about it (say, when using jQuery).
     </p>
     <%= codeFor('objects_reserved') %>
+    <p>
+      CoffeeScript has a shortcut for creating objects when you want the key
+      to be set with a variable of the same name.
+    </p>
+    <%= codeFor('objects_shorthand') %>
 
     <p>
       <span id="lexical-scope" class="bookmark"></span>
