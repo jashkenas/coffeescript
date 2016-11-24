8fdbd1e383d70882105fe0dd62ca140c6cca598b
Gregory Huczynski <gh_online@me.com>
2016-11-24 08:29:12 +0000

    Add note re checking runtime for tagged template literals

--- documentation/index.html.js
+++ documentation/index.html.js
@@ -117,8 +117,10 @@
       that runs in every JavaScript runtime, but there are exceptions. Use
       <a href="#generator-functions">generator functions</a> only if you know that your
       <a href="http://kangax.github.io/compat-table/es6/#test-generators">target
-      runtimes can support them</a>. If you use <a href="#modules">modules</a>, you
-      will need to <a href="#modules-note">use an additional tool to resolve them</a>.
+      runtimes can support them</a>. Same for
+      <a href="#tagged-template-literals">tagged template literals</a>: check the
+      <a href="http://kangax.github.io/compat-table/es6/#test-template_literals">runtime</a>.
+      If you use <a href="#modules">modules</a>, you will need to <a href="#modules-note">use an additional tool to resolve them</a>.
     </p>
 
     <p>
