70fddc14001d236dacdc2034e923f900441d1fda
Geoffrey <webmaster@geoffreybooth.com>
2016-11-25 21:32:32 -0800

    More clarity on what CoffeeScript is doing versus what the runtime is doing, and emphasize runtimes vs Babel/Traceur

--- documentation/index.html.js
+++ documentation/index.html.js
@@ -995,17 +995,20 @@ Block
       <b class="header">Tagged Template Literals</b>
       CoffeeScript supports
       <a href="https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Template_literals#Tagged_template_literals">ES2015 tagged template literals</a>,
-      which enable customized string interpolation.  If you immediately prefix a string
-      with a function (no space between the two), Coffeescript will interpret this 'function
-      plus string' combination as an ES2015 tagged template literal, and
+      which enable customized string interpolation. If you immediately prefix a string
+      with a function name (no space between the two), CoffeeScript will output this
+      'function plus string' combination as an ES2015 tagged template literal, which will
       <a href="https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Template_literals#Tagged_template_literals">behave accordingly</a>:
       the function is called, with the parameters being the input text and expression parts
       that make up the interpolated string. The function can then assemble these parts
-      into an output string, providing custom string interpolation. Be aware that the
-      CoffeeScript compiler is converting your CoffeeScript code into an actual Javascript ES2015
-      tagged template literal, so your target Javascript runtime must support this feature for your code
-      to work. Tools like <a href="http://babeljs.io/">Babel</a> or
-      <a href="https://github.com/google/traceur-compiler">Traceur Compiler</a> can help with this.
+      into an output string, providing custom string interpolation.
+    </p>
+    <p>
+      Be aware that the CoffeeScript compiler is outputting ES2015 syntax for this feature,
+      so your target JavaScript runtime(s) must support this syntax for your code to work;
+      or you could use tools like <a href="http://babeljs.io/">Babel</a> or
+      <a href="https://github.com/google/traceur-compiler">Traceur Compiler</a> to convert
+      this ES2015 syntax into compatible JavaScript.
     </p>
     <%= codeFor('tagged_template_literals', 'greet("greg", "awesome")') %>
 
